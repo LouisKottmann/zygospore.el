1095dc4453af91e466576dd5c28a9fc68391eaea
Louis Kottmann <louis.kottman@cpexterne.org>
2014-07-02 15:07:36 +0200

    added commentary for MELPA

--- zygospore.el
+++ zygospore.el
@@ -21,6 +21,14 @@
 
 ;;; Commentary:
 
+;;; This package allows you to press C-x 1 again to restore windows
+
+;;; Install:
+;;; Test it out with M-x zygospore-toggle-delete-other-windows RET (twice, on a frame with multiple windows)
+
+;;; To make it permanent, add this line to your init.el:
+;;; (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
+
 ;;; Code:
 
 (defvar zygospore-spore-formation-register-name
